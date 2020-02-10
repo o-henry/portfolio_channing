f902757a98153012835207d7fd5692fe6b9f6c83
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2020-02-10 13:41:18 +0900

    base portfolio setting

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="About">
+      <div className="About-Head">
+        Hello
+        <div className="About-Body">
+          동료들과의 협업을 좋아하고, 소통하는 개발자 입니다. 고안한 로직에 따라
+          구현되는 개발을 즐기며 다양한 분야에 관심이 많아 배우는걸 즐기고
+          있습니다. 또, 창의적인 일을 좋아하며 아이디어를 기록하고, 실제 구체화
+          가능한지에 대한 고민을 하곤 합니다
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
