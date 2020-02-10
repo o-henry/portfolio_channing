0a9146e24d285f22be69ae5d97f393895b2a23e1
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2020-02-10 21:40:59 +0900

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,25 +16,46 @@ const About = () => {
     <div className="About">
       <img src={me} alt="Channing" />
       <div className="Bio">
-        <br></br><div className="Header">EDUCATION AND ACTIVITIES</div>
+        <div className="Header">EDUCATION AND ACTIVITIES</div>
         <div className="education">
-          Code States(bootcamp) Immersive Course 14 ( 2019.07.22 - 2019.10.19 )
-          <br></br>AWS 101 세미나 ( 2019.10.14 )<br></br>AWS 201 세미나 (
-          2019.10.28 )<br></br> AWS 웨비나 ( 2019.11.14 )
+          <div className="text">
+            코드 스테이츠(부트캠프) Immersive Course 14 | 2019
+            <br />
+            AWS 101, 201 Seminar | 2019
+            <br />
+            2019 AWS Webinar | 2019
+          </div>
         </div>
         <div className="Header">SKILLS</div>
         <span className="subtitle">PROGRAMMING LANGUAGES & TOOLS</span>
         <div className="Icon">
-          <FaAws size={50} />
-          <FaNode size={50} />
-          <FaReact size={50} />
-          <FaHtml5 size={50} />
-          <FaCss3Alt size={50} />
-          <FaJsSquare size={50} />
-          <DiMysql size={50} />
-          <DiMongodb size={50} />
+          <div className="InnerPosition">
+            <FaAws size={50} />
+          </div>
+          <div className="InnerPosition">
+            <FaNode size={50} />
+          </div>
+          <div className="InnerPosition">
+            <FaJsSquare size={50} />
+          </div>
+          <div className="InnerPosition">
+            <FaReact size={50} />
+          </div>
+          <div className="InnerPosition">
+            <FaHtml5 size={50} />
+          </div>
+          <div className="InnerPosition">
+            <FaCss3Alt size={50} />
+          </div>
+          <div className="InnerPosition">
+            <DiMysql size={50} />
+          </div>
+          <div className="InnerPosition">
+            <DiMongodb size={50} />
+          </div>
         </div>
         <span className="subtitle">PROJECT MANAGEMENT</span>
+        <div className="InnerText">Agile Development & Scrum</div>
       </div>
     </div>
   );
