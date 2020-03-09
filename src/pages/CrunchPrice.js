630a26c6610d917f58b6f79aa7892055f1a310a0
Channing <cksgod16@gmail.com>
2020-03-09 15:06:34 +0900

    modi

--- src/pages/CrunchPrice.js
+++ src/pages/CrunchPrice.js
@@ -16,7 +16,7 @@ export default function CrunchPrice() {
       <div className="Half">
         <div className="Content">
           <b>크런치 프라이스</b> 기업협업 프로젝트 2019 <br></br>| Back-End |
-          AWS | Django | Docker |<br></br>
+          AWS |<br></br>
           <br></br> AWS Personalize를 활용하여 MD의 개입없이<br></br>{" "}
           인공지능으로 사용자 개개인 에게 맞는 상품 추천 <br></br>시스템을
           구현했습니다.
